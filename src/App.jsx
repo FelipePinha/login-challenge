75fc160f2fb17eecbd50b32afa2ad681c4df0ac6
Felipe Carvalho <felipe_pc7@hotmail.com>
2023-11-27 18:57:07 -0300

    done todo 3

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { useState } from 'react';
 // Tarefas:
 // *todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
 // *todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
-// todo - Desabilite o botão de Login equanto você está executando o login.
+// *todo - Desabilite o botão de Login equanto você está executando o login.
 // *todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
 // *todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
 
@@ -18,18 +18,22 @@ export default function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
+    const [isFetching, setIsFetching] = useState(false);
 
     const handleLogin = () => {
+        console.log('submitted');
         setError('');
 
+        setIsFetching(true);
         login({ email, password })
             .then(() => {
-                setEmail('');
-                setPassword('');
                 alert('Usuário Logado!');
             })
             .catch(error => {
                 setError(error.message);
+            })
+            .finally(() => {
+                setIsFetching(false);
             });
     };
 
@@ -70,7 +74,10 @@ export default function LoginForm() {
                 </div>
 
                 <div className="button">
-                    <button onClick={handleLogin} disabled={!email || password.length < 6}>
+                    <button
+                        onClick={handleLogin}
+                        disabled={!email || password.length < 6 || isFetching}
+                    >
                         Login
                     </button>
                 </div>
